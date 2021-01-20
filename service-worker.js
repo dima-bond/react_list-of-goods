cdddd4640bd2d33496c812bfd11e362f89d68f3b
Dima Bond <neverpooh@gmail.com>
2021-01-20 19:48:34 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.5fce67c7b18507d0ff862ac235ed226f.js"
 );
 
 self.addEventListener('message', (event) => {
